3a7c762c2fd7e2baeb718d6006e236f393d2f4d8
Ding Ze Yang <dzey0001@admin.monash.edu.my>
2024-06-20 20:20:31 +0800

    Update listen2.py

--- listen2.py
+++ listen2.py
@@ -1,5 +1,5 @@
 #from simple_pid import PID
-from picamera2 import PiCamera2
+from picamera2 import Picamera2, Preview
 from flask import Flask, Response, request
 #from gpiozero import Robot, Motor, DigitalInputDevice
 import io
