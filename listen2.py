01c39fc222795b6785fdc4405619d0cfcf877757
Ding Ze Yang <zeyangding96@gmail.com>
2024-06-21 11:57:49 +0800

    Update listen2.py

--- listen2.py
+++ listen2.py
@@ -48,7 +48,7 @@ def move():
     l_val, r_val = request.args.get('l_val'), request.args.get('r_val')
     l_val, r_val = float(l_val), float(r_val)
     mbot.value = (l_val, r_val)
-    
+    return None
 
 
 # Run Flask in a separate thread
