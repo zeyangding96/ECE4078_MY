080d1a1baba94f18503b3f06279cd5e673c426e4
Ding Ze Yang <zeyangding96@gmail.com>
2024-06-24 15:18:51 +0800

    Update listen2.py

--- listen2.py
+++ listen2.py
@@ -1,4 +1,4 @@
-# from simple_pid import PID
+from simple_pid import PID
 from picamera2 import Picamera2, Preview
 from flask import Flask, Response, request
 from gpiozero import Robot, Motor, DigitalInputDevice
@@ -20,24 +20,25 @@ enc_b = 16
 # Initialize robot and encoders
 mbot = Robot(right=Motor(forward=in1, backward=in2, enable=ena), left=Motor(forward=in3, backward=in4, enable=enb))
 
-### Initialize PID controllers for wheels
-# left_encoder = DigitalInputDevice(enc_a)
-# right_encoder = DigitalInputDevice(enc_b)
-# def left_tick():
-    # global left_count
-    # left_count += 1
+# Initialize PID controllers for wheels
+left_encoder = DigitalInputDevice(enc_a)
+right_encoder = DigitalInputDevice(enc_b)
+def left_tick():
+    global left_count
+    left_count += 1
     
-# def right_tick():
-    # global right_count
-    # right_count += 1
+def right_tick():
+    global right_count
+    right_count += 1
 
-# left_encoder.when_activated = left_tick
-# right_encoder.when_activated = right_tick
-# left_count = 0
-# right_count = 0
-# pid_left = PID(1, 0.1, 0.05, setpoint=0)
-# pid_right = PID(1, 0.1, 0.05, setpoint=0)
-# flag_pid = 1
+left_encoder.when_activated = left_tick
+right_encoder.when_activated = right_tick
+left_count = 0
+right_count = 0
+pid_left = PID(1, 0.1, 0.05, setpoint=0)
+pid_right = PID(1, 0.1, 0.05, setpoint=0)
+flag_pid = 0
+flag_forward = 1
 
 # Initialize the PiCamera
 picam2 = Picamera2()
@@ -56,86 +57,84 @@ def capture_image():
     stream.seek(0)
     return Response(stream, mimetype='image/jpeg')
     
-@app.route('/move')
-def move():
-    l_val, r_val = request.args.get('l_val'), request.args.get('r_val')
-    l_val, r_val = float(l_val), float(r_val)
-    mbot.value = (l_val, r_val)
-    return ""
-    
 # @app.route('/move')
 # def move():
-    # global flag_pid
-    
     # l_val, r_val = request.args.get('l_val'), request.args.get('r_val')
     # l_val, r_val = float(l_val), float(r_val)
+    # mbot.value = (l_val, r_val)
+    # return ""
+    
+@app.route('/move')
+def move():
+    global flag_pid
+    
+    l_val, r_val = request.args.get('l_val'), request.args.get('r_val')
+    l_val, r_val = float(l_val), float(r_val)
     # print(l_val, r_val)
     
     # if stop or turn
-    # if (l_val == 0 and r_val == 0) or (l_val > 0 and r_val < 0) or (l_val < 0 and r_val > 0):
-        # print('stop')
-        # flag_pid = False
-        # time.sleep(0.1)
-        # mbot.value = (l_val, r_val)
+    if (l_val == 0 and r_val == 0) or (l_val > 0 and r_val < 0) or (l_val < 0 and r_val > 0):
+        print('stop')
+        flag_pid = False
+        time.sleep(0.1)
+        mbot.value = (l_val, r_val)
         
     
     # if forward
-    # elif (l_val > 0 and r_val > 0) and not flag_pid:
-        # print('forward')
-        # flag_pid = True
-        # move_robot(forward=True)
+    elif (l_val > 0 and r_val > 0) and not flag_pid:
+        flag_forward = True
+        flag_pid = True
     
     # if backward
-    # elif (l_val < 0 and r_val < 0) and not flag_pid:
-        # flag_pid = True
-        # move_robot(forward=False)  
+    elif (l_val < 0 and r_val < 0) and not flag_pid:
+        flag_forward = False
+        flag_pid = True
     
-    # return ""
+    return ""
     
-# def move_robot(forward=True):
-    # global stop_event
-    # stop_event.clear()
+def move_robot():
     
-    # global left_count, right_count, flag_pid
-    # left_count = 0
-    # right_count = 0
+    global left_count, right_count, flag_pid, flag_forward
     
-    # pid_left.setpoint = 9999
-    # pid_right.setpoint = 9999
+    pid_left.setpoint = 9999
+    pid_right.setpoint = 9999
     
-    # while flag_pid:
-        # l_val = pid_left(left_count)
-        # r_val = pid_right(right_count)
+    while True:
+    
+        # reset pid controller (when robot stops or turn)
+        if not flag_pid:
+            left_count = 0
+            right_count = 0
+            
+        else:
         
-        # l_val = max(min(l_val, 1), -1)
-        # r_val = max(min(r_val, 1), -1)
-        # print(l_val, r_val)
+            l_val = pid_left(left_count)
+            r_val = pid_right(right_count)
+            
+            l_val = max(min(l_val, 1), -1)
+            r_val = max(min(r_val, 1), -1)
+            # print(l_val, r_val)
         
-        # if forward:
-            # mbot.value = (l_val, r_val)
-        # else:
-            # mbot.value = (-l_val, -r_val)
+            if flag_forward:
+                mbot.value = (l_val, r_val)
+            else:
+                mbot.value = (-l_val, -r_val)
         
-        # time.sleep(0.01)
-    
-    # mbot.stop()
-
+        time.sleep(0.01)
 
 
 # Run Flask in a separate thread
 def run_flask():
     app.run(host='0.0.0.0', port=8000)
 
-# stop_event = threading.Event()
 flask_thread = threading.Thread(target=run_flask)
 flask_thread.daemon = True
 flask_thread.start()
 
 try:
     while True:
-        time.sleep(1)
+        move_robot()
 except KeyboardInterrupt:
-    # stop_event.set()
     mbot.stop()
     picam2.stop()
     print("Program interrupted by user.")
