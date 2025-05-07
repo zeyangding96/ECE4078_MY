9c4d692b7c4632c7bd556b97d23254af96da1923
Ding Ze Yang <zeyangding96@gmail.com>
2025-05-08 01:55:13 +0800

    Update listen2.py

--- listen2.py
+++ listen2.py
@@ -74,10 +74,12 @@ def setup_gpio():
 def left_encoder_callback(channel):
     global left_count
     left_count += 1
+    print(left_count)
 
 def right_encoder_callback(channel):
     global right_count
     right_count += 1
+    print(right_count)
 
 def set_motors(left, right):
     if left >= 0:
